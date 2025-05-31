6dc730e03cf4a3f9ba23d0df57866f362a0c9a53
Shreyam Sharma <rajroy10008@gmail.com>
2025-05-31 16:47:04 +0530

    added splitted animation

--- script.js
+++ script.js
@@ -4,23 +4,46 @@ function bearkTheText() {
     // var h1Text = document.querySelector("h1").textContent //works same
 
     var splittedText = h1Text.split("") //splitting the text on the basis of nothing
+    var halfValue = splittedText.length/2
 
     var clutter = ""
 
-    splittedText.forEach((elem)=>{
-        clutter = clutter + `<span>${elem}</span>`
-        // clutter += elem //works same
+    splittedText.forEach((elem, idx)=>{
+        if(idx<halfValue){
+            clutter = clutter + `<span class="a">${elem}</span>`
+            // clutter += elem //works same
+        }
+        else{
+            clutter = clutter + `<span class="b">${elem}</span>`
+            // clutter += elem //works same
+        }
     })
     h1.innerHTML = clutter
     
 }
 bearkTheText()
 
-gsap.from("h1 span",{
-    y:70,
-    duration:0.8,
+// gsap.from("h1 span",{ //simple animations
+//     y:70,
+//     duration:0.8,
+//     delay:0.5,
+//     opacity:0,
+//     stagger:0.2 //left-to-right animation
+//     // stagger:-0.2 //right-to-left animation
+// })
+
+
+gsap.from("h1 .a",{
+    y:50,
+    duration:0.6,
     delay:0.5,
     opacity:0,
-    stagger:0.2 //left-to-right animation
-    // stagger:-0.2 //right-to-left animation
+    stagger:0.15
+})
+gsap.from("h1 .b",{
+    y:50,
+    duration:0.6,
+    delay:0.5,
+    opacity:0,
+    stagger:-0.15
 })
